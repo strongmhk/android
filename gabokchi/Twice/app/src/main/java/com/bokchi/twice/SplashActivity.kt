35cf524cc15481481aefcc581a6f808b90c1588c
strongmhk <kim6562166086@gmail.com>
2023-06-29 17:23:53 +0900

    Add image

--- gabokchi/Twice/app/src/main/java/com/bokchi/twice/SplashActivity.kt
+++ gabokchi/Twice/app/src/main/java/com/bokchi/twice/SplashActivity.kt
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
+@Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+
+        // 3초간 splash 화면 보이기
         Handler().postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
