4b1f0525e0e3365a970aa77f305d6d67272d20a0
strongmhk <kim6562166086@gmail.com>
2023-06-29 15:29:51 +0900

    add image file

--- gabokchi/Bts_image/app/src/main/java/com/bokchi/bts_image/MainActivity.kt
+++ gabokchi/Bts_image/app/src/main/java/com/bokchi/bts_image/MainActivity.kt
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // 화면을 보여주는 코드
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
 }
