8324ca2bbe9f39c29ef1e1e97943d8b20f9e010f
strongmhk <kim6562166086@gmail.com>
2023-06-30 15:43:34 +0900

    Create custom_font project

---
+++ gabokchi/custom_font/app/src/main/java/com/bokchi/custom_font/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bokchi.custom_font
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
